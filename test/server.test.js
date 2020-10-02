a76f8c796ff05b92f5733bbf7cff265d974c0390
choilm <choilmto@gmail.com>
2020-10-02 17:49:05 -0400

    Stub out all endpoints and add tests

--- test/server.test.js
+++ test/server.test.js
@@ -20,3 +20,52 @@ describe("GET", () => {
     expect(response.status).toBe(200);
   });
 });
+
+describe("Get all users", () => {
+  test("when there are zero users", async () => {
+    const response = await request
+      .get("/api/exercise/users")
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+});
+
+describe("Get user log", () => {
+  test("when the log has a count of 2", async () => {
+    const ID = "TEST_ID";
+    const response = await request
+      .get("/api/exercise/log?userId=" + ID)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toBe(ID);
+  });
+});
+
+describe("Post new user", () => {
+  test("when user has valid username", async () => {
+    const USERNAME = "TEST_USERNAME";
+    const response = await request
+      .post("/api/exercise/new-user")
+      .send("username=" + USERNAME)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body.username).toBe(USERNAME);
+  });
+});
+
+describe("Post exercise", () => {
+  test("when valid ID is submitted", async () => {
+    const ID = "TEST_ID";
+    const response = await request
+      .post("/api/exercise/add")
+      .send("userId=" + ID)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toBe(ID);
+  });
+});
