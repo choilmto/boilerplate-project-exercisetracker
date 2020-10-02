dc22bde241b3037b2be04c6e2f88f53e49b681fd
choilm <choilmto@gmail.com>
2020-10-02 17:49:05 -0400

    Stub out all endpoints and add tests

--- server.js
+++ server.js
@@ -17,6 +17,40 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/api/exercise/users", (req, res) => {
+  res.json([]);
+});
+app.get("/api/exercise/log", (req, res) => {
+  res.json({
+    _id: req.query.userId,
+    username: "TEST_USERNAME",
+    count: 2,
+    log: [
+      {
+        description: "TEST_ACTIVITY_1",
+        duration: "TEST_DURATION",
+        date: Date.now(),
+      },
+      {
+        description: "TEST_ACTIVITY_2",
+        duration: "TEST_DURATION",
+        date: Date.now(),
+      },
+    ],
+  });
+});
+app.post("/api/exercise/new-user", (req, res) => {
+  res.json({ username: req.body.username, _id: "TEST_ID" });
+});
+app.post("/api/exercise/add", (req, res) => {
+  res.json({
+    _id: req.body.userId,
+    username: "TEST_USERNAME",
+    date: req.body.date || Date.now(),
+    duration: req.body.duration,
+    description: req.body.description,
+  });
+});
 
 // Not found middleware
 app.use((req, res, next) => {
